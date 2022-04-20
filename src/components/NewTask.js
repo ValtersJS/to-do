b909ab712e41b953a4a1aee1d8605b4709102c4f
ValtersJS <valters.sakarnis@gmail.com>
2022-04-20 17:16:09 +0300

    hey

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,26 +1,10 @@
-import React from 'react'
-
-export default function NewTask({ newTask, handleChange, handleSubmit }) {
-  return(
-    <form onSubmit={handleSubmit}>
-      <input
-        name="title"
-        placeholder="New Task"
-        value={newTask.title || ""}
-        onChange={handleChange}
-      />
-
-      {!newTask.title ? null : (
-        <>
-          <textarea
-            name="description"
-            placeholder='description'
-            value={newTask.description || ""}
-            onChange={handleChange}
-          />
-          <button type="submit">Add Task</button>
-        </>
-      )}
-    </form>
+const NewTask = () => {
+  return (
+    <div>
+      <input />
+      <h1>Hello</h1>
+    </div>
   );
 }
+
+export default NewTask;
